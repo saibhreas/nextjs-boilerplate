9418fcee08dfae27eb510e9bdce76c6ad9cbe3a0
siobhan <sr@saibhreas.com>
2024-06-30 14:25:40 -0400

    Best ost strightforward tutorial sof ar.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,11 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Space_Grotesk } from "next/font/google";
 import "./globals.css";
+import { ThemeProvider } from "@/components/theme-provider";
 
-const inter = Inter({ subsets: ["latin"] });
+
+
+const spaceGrotesk = Space_Grotesk({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Saibhreas Portfolio",
@@ -15,9 +18,17 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}
-      bg-zinc-100 text-zinc-900>{children}</body>
+    <html lang="en" suppressHydrationWarning>
+      <body className={spaceGrotesk.className}>
+        <ThemeProvider
+            attribute="class"
+            defaultTheme="dark"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {children}
+        </ThemeProvider>    
+      </body>
     </html>
   );
 }
