f439733c00d27fa4d1b1ab7ba17ede57ed83c09a
siobhan <sr@saibhreas.com>
2024-06-30 15:37:54 -0400

    up to adding icons. good but leaves out some detiail.  it is just rpetion at this point

--- app/(home)/components/Navbar.tsx
+++ app/(home)/components/Navbar.tsx
@@ -1,10 +1,41 @@
+import Link from 'next/link'
 import React from 'react'
 
+import { SiLinkedin, SiGithub } from "react-icons/si";
+
 export default function Navbar() {
+  const socials = [
+    {
+      Link: 'https://github.com/saibhreas',
+      Label: 'Github',
+      Icon: SiGithub,
+    },
+    {
+      Link: 'https://www.linkedin.com/in/siobhanknuttel',
+      Label: 'Linkedin',
+      Icon: SiLinkedin ,
+    },
+  ];
   return (
+
     <div>
-      Navbar
-      
+      <h1>
+        Saibhreas
+      </h1>
+      <div>
+        {socials.map((social, index)=>{
+          const Icon = social.Icon
+
+          return (
+            <Link href={social.Link} 
+              key={index} 
+              aria-label={social.label}>
+              <Icon  />
+            </Link>
+          )
+        })}
+      </div>
+
     </div>
   )
 }
