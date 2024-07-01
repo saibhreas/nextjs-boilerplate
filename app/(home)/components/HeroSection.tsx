187e4c50ff7706e69416fa5519379d0c586f57ba
siobhan <sr@saibhreas.com>
2024-06-30 21:20:19 -0400

    going well taking break

--- app/(home)/components/HeroSection.tsx
+++ app/(home)/components/HeroSection.tsx
@@ -4,28 +4,46 @@ import { MdAlternateEmail } from "react-icons/md";
 
 export default function HeroSection() {
   return (
-    <div>
-      <div>
-        <h1>
+    <div className=' min-h-[60vh] flex items-center justify-between'>
+      <div className='space-y-10'>
+        <h1 className='text-4xl lg:text-7xl font-bold'>
           This is my hero here section!
           <br />
-          <span className=" underline underline-offset-5 decoration-yellow-400 text-2xl padding-top: 15px padding-bottom: 15px">
+          <span className=" underline underline-offset-10 decoration-yellow-400 text-3xl padding-top: 15px padding-bottom: 15px">
             {"This is where Snappy Jargon sits."}
           </span>
         </h1>
-       <br />  
-       <br />  
-       <p>
+ 
+       <p className="; md:w-96 text-lg text-gray-300">
           {
-            "Here you will find the elucid ramblings of Lorum an Epsum.  The two authors are better known for their prolific Dadaesque art installations on web apps the world over.  However, here you will find a more readable prose (sidebar- not much more meaningful though)."
+            "Here you will find the elucid ramblings of Lorum and Epsum.  The two authors are better known for their prolific Dadaesque art installations on web apps the world over.  However, here you will find a more readable prose (sidebar- not much more meaningful though)."
           }
         </p>
-        <br />
-        <Link href="mailto:fullstack@saibhreas.com">
+
+        <Link href="mailto:fullstack@saibhreas.com" className='inline-block'>
           <div className=' text-1xl flex padding-top: 5px; text-cyan-400'>
-          fullstack<MdAlternateEmail />saibhreas.com
+            <h1 className='text-l font bold'>
+              fullstack <MdAlternateEmail />saibhreas.com</h1>
           </div>
         </Link>
+      </div>
+      <div>
+
+        <div className='w-72 h-72 spacey-3 -rotate-[36deg]'>
+          <div className='flex gap-3 translate-x-8'>
+            <div className=' w-32 h-32 rounded-full bg-yellow-400'></div>
+            <div className=' w-32 h-32 rounded-2xl bg-cyan-500'></div>
+          </div>
+
+          <div className='flex gap-3 -translate-x-8'>
+            <div className=' w-32 h-32 rounded-2xl bg-cyan-600'></div>
+            <div className=' w-32 h-32 rounded-full bg-lime-400'></div>
+          </div>
+
+          
+
+
+        </div>
       </div>
     </div>
   );
