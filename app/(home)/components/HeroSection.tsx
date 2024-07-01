7b06d4839af16dcefadd3cd400e9c5e16b165afe
siobhan <sr@saibhreas.com>
2024-06-30 23:39:34 -0400

    adding effect next.  jsut worked on spacing and sytle

--- app/(home)/components/HeroSection.tsx
+++ app/(home)/components/HeroSection.tsx
@@ -1,29 +1,30 @@
 import React from "react";
 import Link from 'next/link'
-import { MdAlternateEmail } from "react-icons/md";
+import { MdEmail } from "react-icons/md";
 
 export default function HeroSection() {
   return (
-    <div className=' min-h-[60vh] flex items-center justify-between'>
-      <div className='space-y-10'>
+    <div className=' min-h-[60vh] flex flex-col-reverse gap-12 lg:flex-row items-center justify-between'>
+      <div className='space-y-10 text-center lg:text-left'>
         <h1 className='text-4xl lg:text-7xl font-bold'>
           This is my hero here section!
           <br />
-          <span className=" underline underline-offset-10 decoration-yellow-400 text-3xl padding-top: 15px padding-bottom: 15px">
+          <span className=" underline underline-offset-10 decoration-amber-400 text-3xl padding-top: 15px padding-bottom: 15px">
             {"This is where Snappy Jargon sits."}
           </span>
         </h1>
  
-       <p className="; md:w-96 text-lg text-gray-300">
+       <p className="; md:w-96 text-lg  text-gray-300">
           {
             "Here you will find the elucid ramblings of Lorum and Epsum.  The two authors are better known for their prolific Dadaesque art installations on web apps the world over.  However, here you will find a more readable prose (sidebar- not much more meaningful though)."
           }
         </p>
 
         <Link href="mailto:fullstack@saibhreas.com" className='inline-block'>
-          <div className=' text-1xl flex padding-top: 5px; text-cyan-400'>
-            <h1 className='text-l font bold'>
-              fullstack <MdAlternateEmail />saibhreas.com</h1>
+          <div >
+            <h1 className='text-2xl font bold hover:text-cyan-500 '> Contact </h1>
+            <div className=' w-40 h-2 bg-amber-400 rounded-full'></div>
+            <div className=' w-40l h-2 bg-cyan-400 rounded-full translate-x-3'></div>
           </div>
         </Link>
       </div>
@@ -31,13 +32,13 @@ export default function HeroSection() {
 
         <div className='w-72 h-72 spacey-3 -rotate-[36deg]'>
           <div className='flex gap-3 translate-x-8'>
-            <div className=' w-32 h-32 rounded-full bg-yellow-400'></div>
-            <div className=' w-32 h-32 rounded-2xl bg-cyan-500'></div>
+            <div className=' w-32 h-32 rounded-full bg-amber-300 '></div>
+            <div className=' w-32 h-32 rounded-2xl bg-cyan-500 '></div>
           </div>
 
           <div className='flex gap-3 -translate-x-8'>
             <div className=' w-32 h-32 rounded-2xl bg-cyan-600'></div>
-            <div className=' w-32 h-32 rounded-full bg-lime-400'></div>
+            <div className=' w-32 h-32 rounded-full bg-amber-300'></div>
           </div>
 
           
--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -6,7 +6,7 @@ import HeroSection from './components/HeroSection'
 export default function page() {
   return (
     <div className=' min-h-screen bg-black'>
-      <div className='max-w-7xl mx-auto'>
+      <div className='max-w-7xl mx-auto p-5 overflow-hidden'>
           <Navbar  />
           <HeroSection />
       </div>
