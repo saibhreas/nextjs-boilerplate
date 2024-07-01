a2c7a6a14a2181a64098d4b999195a158703d475
siobhan <sr@saibhreas.com>
2024-06-30 20:18:25 -0400

    Hero moving alon. pretty basic. learning tailwinds"

--- app/(home)/components/HeroSection.tsx
+++ app/(home)/components/HeroSection.tsx
@@ -9,17 +9,20 @@ export default function HeroSection() {
         <h1>
           This is my hero here section!
           <br />
-          <span className=" underline underline-offset-5 decoration-yellow-400 ">
+          <span className=" underline underline-offset-5 decoration-yellow-400 text-2xl padding-top: 15px padding-bottom: 15px">
             {"This is where Snappy Jargon sits."}
           </span>
         </h1>
-        <p>
+       <br />  
+       <br />  
+       <p>
           {
-            "Here you will find the elucid ramblings of Lorum an Epsum.  The two authors are better known for their prolific Dadaesque art installations on web apps the world over.  However, here you will find a more readable prose (sidebar- not much more meaningful)."
+            "Here you will find the elucid ramblings of Lorum an Epsum.  The two authors are better known for their prolific Dadaesque art installations on web apps the world over.  However, here you will find a more readable prose (sidebar- not much more meaningful though)."
           }
         </p>
+        <br />
         <Link href="mailto:fullstack@saibhreas.com">
-          <div className=' text-2xl flex padding-top: 5px; text-cyan-400'>
+          <div className=' text-1xl flex padding-top: 5px; text-cyan-400'>
           fullstack<MdAlternateEmail />saibhreas.com
           </div>
         </Link>
